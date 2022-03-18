46f53a078279d0a9f97c29c099a94a99ebac4d02
adithya-gullapalli <101258623+adithya-gullapalli@users.noreply.github.com>
2022-03-18 10:55:39 +0530

    Update 20220314033015-users-table-up.sql

--- migrations/sqls/20220314033015-users-table-up.sql
+++ migrations/sqls/20220314033015-users-table-up.sql
@@ -4,5 +4,3 @@ CREATE TABLE Users (
     last_name VARCHAR(100),
     password VARCHAR(100)
 );
-
-INTO Users(first_name, last_name, password) VALUES ('Hannes', 'Roth', '$2b$10$5sYpcJRckiAvixaXOD7jU..qOs53ub/iCXxw/GpW3RgVWHNvR6Kci');
